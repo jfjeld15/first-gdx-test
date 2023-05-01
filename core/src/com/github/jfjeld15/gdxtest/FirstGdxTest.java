ce1d210b6804c11b3de7152104159aa10a4f3b6c
jfjeld15 <jfjeld@ualberta.ca>
2023-05-01 13:42:27 -0600

    SETTING UP ANIMATION AND BASIC MOVEMENT

--- core/src/com/github/jfjeld15/gdxtest/FirstGdxTest.java
+++ core/src/com/github/jfjeld15/gdxtest/FirstGdxTest.java
@@ -1,31 +1,97 @@
 package com.github.jfjeld15.gdxtest;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.Rectangle;
 
 public class FirstGdxTest extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private float horizontalSpeed;
+	private Texture idleSheet;
+	private Music laserGun;
+	private OrthographicCamera camera;
+	private SpriteBatch idleBatch;
+	private Animation<TextureRegion> idleAnimation;
+	private Rectangle player;
+	private float stateTime;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// Camera setup (Ensures that the camera is always showing 640x480 area)
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 640, 480);
+
+		idleBatch = new SpriteBatch();
+
+		// Load the images:
+		idleSheet = new Texture(Gdx.files.internal("_Run.png"));
+		TextureRegion[][] tmp = TextureRegion.split(idleSheet, idleSheet.getWidth()/10, idleSheet.getHeight());
+		TextureRegion[] idleFrames = new TextureRegion[10];
+		int index = 0;
+		for (int i=0; i<10; i++) {
+			idleFrames[index++] = tmp[0][i];
+		}
+		idleAnimation = new Animation<>(1/10f, idleFrames);
+		idleBatch = new SpriteBatch();
+		stateTime = 0f;
+
+		// Load the music and sound FX:
+		laserGun = Gdx.audio.newMusic(Gdx.files.internal("Laser Gun.mp3"));
+
+		// Start music playback (And looping)
+		laserGun.setLooping(true);
+		laserGun.play();
+
+		// Set up the player starting point
+		player = new Rectangle();
+		player.x = 640f /2 - 20f/2;
+		player.y = 10;
+		player.width = 20;
+		player.height = 38;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		ScreenUtils.clear(0, 0, 0.2f, 1);  // Dark Blue
+		camera.update();
+		stateTime += Gdx.graphics.getDeltaTime();  // Accumulate elapsed animation time
+
+		idleBatch.setProjectionMatrix(camera.combined);
+		TextureRegion currentFrame = idleAnimation.getKeyFrame(stateTime, true);
+		idleBatch.begin();
+		idleBatch.draw(currentFrame, player.x, player.y);
+		idleBatch.end();
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) horizontalSpeed = 0;
+		if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) horizontalSpeed = 0;
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			if (horizontalSpeed != -10f) {
+				horizontalSpeed -= 0.2f;
+			}
+		}
+		else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			if (horizontalSpeed != 10f) {
+				horizontalSpeed += 0.2f;
+			}
+		}
+		else {horizontalSpeed = 0;}
+		player.x += (int) horizontalSpeed;
+		if(player.x < 0) player.x = 0;
+		if(player.x > 640-20) player.x = 640-20;
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		idleBatch.dispose();
+		idleSheet.dispose();
+		laserGun.dispose();
 	}
 }
--- desktop/src/com/github/jfjeld15/gdxtest/DesktopLauncher.java
+++ desktop/src/com/github/jfjeld15/gdxtest/DesktopLauncher.java
@@ -2,14 +2,15 @@ package com.github.jfjeld15.gdxtest;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.github.jfjeld15.gdxtest.FirstGdxTest;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("first-gdx-test");
+		config.setForegroundFPS(30);
+		config.setTitle("BALLS");
+		config.setResizable(false);
+		config.setWindowedMode(640, 480);
 		new Lwjgl3Application(new FirstGdxTest(), config);
 	}
 }
