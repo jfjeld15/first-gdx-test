5f13617aff7655d64ec27bce762ce3e379d814c5
jfjeld15 <jfjeld@ualberta.ca>
2023-05-01 10:36:01 -0600

    Initial Commit

---
+++ android/src/com/github/jfjeld15/gdxtest/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.jfjeld15.gdxtest;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.jfjeld15.gdxtest.FirstGdxTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FirstGdxTest(), config);
+	}
+}
---
+++ desktop/src/com/github/jfjeld15/gdxtest/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.github.jfjeld15.gdxtest;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.github.jfjeld15.gdxtest.FirstGdxTest;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("first-gdx-test");
+		new Lwjgl3Application(new FirstGdxTest(), config);
+	}
+}
